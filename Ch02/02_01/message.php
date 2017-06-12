9cf3aee312b24ea39c9f51b9efeaf8bb94b28537
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2017-06-12 11:31:36 +0300

    adding the ajax php files

--- Ch02/02_01/message.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../../includes/config.php';
-
-try {
-    // prepare email message
-
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
--- Ch06/06_02/begin/process.php
+++
@@ -1,4 +0,0 @@
-<?php
-$expected = ['name', 'email', 'ticket_type', 'dietary_needs', 'interests', 'accept_terms'];
-$required = ['name', 'email', 'accept_terms'];
-
